98ae41206513ca86a9c84410054e8fdf90a762e8
Lu <piece7775@gmail.com>
2022-05-12 14:20:24 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Xarrow, {useXarrow, Xwrapper} from 'react-xarrows';
+import Draggable from 'react-draggable';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const boxStyle = {border: 'grey solid 2px', borderRadius: '10px', padding: '5px', height:'100px', width:'100px'};
+
+const DraggableBox = ({id}) => {
+    const updateXarrow = useXarrow();
+    return (
+        <Draggable onDrag={updateXarrow} onStop={updateXarrow}>
+            <div id={id} style={boxStyle}>
+                {id}
+            </div>
+        </Draggable>
+    );
+};
+
+export default function App() {
+    return (
+        <div style={{display: 'flex', justifyContent: 'space-evenly', height:'200vh', width: '100%'}}>
+            <div style={{ height:'100vh', width: '100%', background:'#DAFF9E' }} > 
+              <Xwrapper>
+                  <DraggableBox id={'elem1'}/>
+                  <DraggableBox id={'elem2'}/>
+                  <Xarrow start={'elem1'} end="elem2"/>
+              </Xwrapper>
+            </div>
+        </div>
+    );
 }
-
-export default App;
